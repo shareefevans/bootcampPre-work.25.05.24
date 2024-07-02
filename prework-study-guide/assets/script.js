51207363b48d814b618e03b2c9948dd3144ce1f4
shareefevans <shareef3vans@gmail.com>
2024-07-03 09:07:27 +1000

    calling javascript functions

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -4,27 +4,30 @@
     - if / else if / else -> if the first statement is false, we move to else if, if that is also false we move to else (it can continue on for a while like this and we can have multiple 'else ifs' within an if statement)
 */
 
-const topic = 'HTML';
-
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Gut!");
-} else if (topic === 'Javascript') {
-    console.log("let's study Javascript");
-} else {
-    console.log('Please try again!');
-}
-
-
-// the above ^ will log: 'Let's study HTML' because the topic equals HTML therefore the first if statement is true and thus logged to the console
-
-// ARRAYS + LOOPS
-
 const topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Gut!");
+    } else if (randomTopic === 'Javascript') {
+        console.log("let's study Javascript");
+    } else {
+        console.log('Please try again!');
+    }
 }
+
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+}
+
+console.log('Here are the topics we learned through Prework');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
